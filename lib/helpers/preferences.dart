3c797e7039c401a20f848867bc9162df4ac3466d
Amaro Suárez <91960825+amarosv@users.noreply.github.com>
2025-01-13 08:40:04 +0100

    Update preferences.dart with doc comments

--- lib/helpers/preferences.dart
+++ lib/helpers/preferences.dart
@@ -1,6 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-// Función que almacena un valor en SharedPreferences
+/// Función que almacena un valor en SharedPreferences <br>
+/// @parama tag Titulo de la etiqueta donde se almacenará el valor <br>
+/// @param value Valor a almacenar
 void saveData({required String tag, required dynamic value}) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
@@ -16,25 +18,33 @@ void saveData({required String tag, required dynamic value}) async {
   }
 }
 
-// Cargar un String desde SharedPreferences
+/// Función que carga un String desde SharedPreferences <br>
+/// @param tag Título de la etiqueta donde está almacenado el String <br>
+/// @return String almacenado en la etiqueta
 Future<String> loadDataString({required String tag}) async {
   final prefs = await SharedPreferences.getInstance();
   return prefs.getString(tag) ?? '';
 }
 
-// Cargar un int desde SharedPreferences
+/// Función que carga un int desde SharedPreferences <br>
+/// @param tag Título de la etiqueta donde está almacenado el int <br>
+/// @return int almacenado en la etiqueta
 Future<int> loadDataInt({required String tag}) async {
   final prefs = await SharedPreferences.getInstance();
   return prefs.getInt(tag) ?? 0;
 }
 
-// Cargar un bool desde SharedPreferences
+/// Función que carga un bool desde SharedPreferences <br>
+/// @param tag Título de la etiqueta donde está almacenado el bool <br>
+/// @return bool almacenado en la etiqueta
 Future<bool> loadDataBool({required String tag}) async {
   final prefs = await SharedPreferences.getInstance();
   return prefs.getBool(tag) ?? false;
 }
 
-// Cargar un double desde SharedPreferences
+/// Función que carga un double desde SharedPreferences <br>
+/// @param tag Título de la etiqueta donde está almacenado el double <br>
+/// @return double almacenado en la etiqueta
 Future<double> loadDataDouble({required String tag}) async {
   final prefs = await SharedPreferences.getInstance();
   return prefs.getDouble(tag) ?? 0;
