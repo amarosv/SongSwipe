4176fe2e8b412a5cfa0843b0f21061a635de362a
Amaro Suárez <91960825+amarosv@users.noreply.github.com>
2025-01-13 08:43:07 +0100

    Update app_theme.dart with doc comments

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -8,31 +8,34 @@ const colorList = {
   'Pink': Colors.pink
 };
 
+/// Clase donde se almacena la configuración del tema de la aplicación
 class AppTheme {
-  // Atributo que almacena el índice del color seleccionado
+  /// Atributo que almacena el índice del color seleccionado
   final int selectedColor;
 
-  // Atributo que almacena si el modo oscuro está activado
+  /// Atributo que almacena si el modo oscuro está activado
   final bool isDarkMode;
 
-  // Atributo que almacena si se está usando el tema del dispositivo
+  /// Atributo que almacena si se está usando el tema del dispositivo
   final bool isUsingSystem;
 
-  // Constructor con aserciones
+  /// Constructor con aserciones
   AppTheme({this.selectedColor = 0, this.isDarkMode = false, this.isUsingSystem = false})
       : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
         assert(selectedColor < colorList.length,
             'Selected colormut be less or equal than ${colorList.length - 1}');
 
-  // Devuelve el tema con las configuraciones cargadas
+  /// Función que devuelve el tema con las configuraciones cargadas <br>
+  /// @return Devuelve el tema con las configuraciones cargadas
   ThemeData getTheme() => ThemeData(
       useMaterial3: true,
       brightness: isDarkMode ? Brightness.dark : Brightness.light,
       colorSchemeSeed: colorList.values.toList()[selectedColor],
       appBarTheme: const AppBarTheme(centerTitle: true));
 
-  // Método que permite crear una copia de la instancia actual de AppTheme,
-  // pero con la posibilidad de sobrescribir algunos de sus valores.
+  /// Método que permite crear una copia de la instancia actual de AppTheme,
+  /// pero con la posibilidad de sobrescribir algunos de sus valores.
+  /// @return Copia del tema de la app
   AppTheme copyWith({int? selectedColor, bool? isDarkMode, bool? isUsingSystem}) => AppTheme(
       selectedColor: selectedColor ?? this.selectedColor,
       isDarkMode: isDarkMode ?? this.isDarkMode,
